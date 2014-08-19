aa69344080aeaaac088da8150040e3be9af07958
Aracon <info@coding99.ru>
2014-08-19 12:07:51 +0400

    Добавил файлы проекта
    Первичный коммит файлов проекта "Шахматы" в репозиторий

---
+++ ChessNoOop/Pawn.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chess
+{
+    class Pawn
+    {
+        FigureColor color;
+        int col;
+        int row;
+
+        public bool CheckMove(Move m)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
